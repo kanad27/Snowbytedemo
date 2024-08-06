2cb56699cc0650b4c6f1004a0a26732072ef76c4
kanad27 <kanad27@gmail.com>
2024-08-07 00:12:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import vega_datasets as vega
 from vega_datasets import data
 from snowflake.snowpark.context import get_active_session
 
